80d832e1e9b151dfbbc3c3a1816acd9451e1596c
Gino Prasad <ginoprasad@gmail.com>
2022-11-07 10:16:30 -0800

    Fixed sample plot bug where an amplicon in multiple locations in the chromosome is merged.

--- caper/caper/sample_plot.py
+++ caper/caper/sample_plot.py
@@ -49,12 +49,8 @@ def plot(sample, sample_name, project_name, filter_plots=True):
         chromosomes = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11",
         "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22",
         "X", "Y")
-
-    a = 1
+    
     cmap = cm.get_cmap('Spectral', len(amplicon['AA amplicon number'].unique()))
-    cmap = cmap(np.arange(len(amplicon['AA amplicon number'].unique())))
-    cmap[:,0:3] *= a 
-    cmap = ListedColormap(cmap)
     amplicon_colors = [f"rgba({', '.join([str(val) for val in cmap(i)])})" for i in range(cmap.N)]
 
     rows = (len(chromosomes) // 4) + 1 if len(chromosomes) % 4 else len(chromosomes) // 4
@@ -83,10 +79,20 @@ def plot(sample, sample_name, project_name, filter_plots=True):
         x_array = []
         y_array = []
         for i in range(len(dfs[key])):
+            #CN Start
             x_array.append(dfs[key].iloc[i, 1])
-            x_array.append(dfs[key].iloc[i, 2])
             y_array.append(dfs[key].iloc[i, 4])
-            y_array.append(dfs[key].iloc[i, 4])
+
+            if dfs[key].iloc[i, 2] - dfs[key].iloc[i, 1] > 10000000:
+                divisor = (dfs[key].iloc[i, 2] - dfs[key].iloc[i, 1]) / 10
+                for j in range(1, 11):
+                    x_array.append(dfs[key].iloc[i, 1] + divisor * j)
+                    y_array.append(dfs[key].iloc[i, 4])
+            else:
+                #CN End
+                x_array.append(dfs[key].iloc[i, 2])
+                y_array.append(dfs[key].iloc[i, 4])
+            #Drop off
             x_array.append(dfs[key].iloc[i, 2])
             y_array.append(np.nan)
 
@@ -124,32 +130,32 @@ def plot(sample, sample_name, project_name, filter_plots=True):
                             row['Feature Position'] = int(locsplit[1]) + offset
                             row['Y-axis'] = 95
                         amplicon_df = pd.concat([row, amplicon_df])
+                    amplicon_df['Feature Start Position'] = amplicon_df['Feature Start Position'].astype(float)
+                    amplicon_df['Feature End Position'] = amplicon_df['Feature End Position'].astype(float)
+                    amplicon_df['Feature Position'] = amplicon_df['Feature Position'].astype(float)
+                    amplicon_df['Feature Maximum Copy Number'] = amplicon_df['Feature maximum copy number'].astype(float)
+                    amplicon_df['Feature Median Copy Number'] = amplicon_df['Feature median copy number'].astype(float)
+                    amplicon_df = amplicon_df.round(decimals=2)
+                    for i in range(len(amplicon_df['AA amplicon number'].unique())):
+                        number = amplicon_df['AA amplicon number'].unique()[i]
+                        per_amplicon = amplicon_df[amplicon_df['AA amplicon number'] == number]
+                        
+                        show_legend = number not in seen
+                        seen.add(number)
+                        
+                        fig.add_trace(go.Scatter(x = per_amplicon['Feature Position'], y = per_amplicon['Y-axis'], 
+                                customdata = amplicon_df, mode='lines',fill='tozeroy', hoveron='points+fills', hovertemplate=
+                                '<br><i>Feature Classification:</i> %{customdata[3]}<br>' + 
+                                '<i>%{customdata[16]}:</i> %{customdata[17]} - %{customdata[18]}<br>' +
+                                '<i>Oncogenes:</i> %{customdata[5]}<br>'+
+                                '<i>Feature Maximum Copy Number:</i> %{customdata[9]}<br>'
+                                f'<b class="/{project_data_dir}/AA_outputs/{sample_name}/{sample_name}_AA_results/{sample_name}_amplicon{number}.png">Click to Download Amplicon {number} PNG</b>',
+                                name = '<b>Amplicon ' + str(number) + '</b>', opacity = 0.3, fillcolor = amplicon_colors[amplicon_numbers.index(number)],
+                                line = dict(color = amplicon_colors[amplicon_numbers.index(number)]), showlegend=show_legend, legendrank=number, text='hallo'), row = rowind, col = colind)
+                        fig.update_traces(textposition="bottom right")
 
-                            
-        if not amplicon_df.empty:
-            amplicon_df['Feature Start Position'] = amplicon_df['Feature Start Position'].astype(float)
-            amplicon_df['Feature End Position'] = amplicon_df['Feature End Position'].astype(float)
-            amplicon_df['Feature Position'] = amplicon_df['Feature Position'].astype(float)
-            amplicon_df['Feature Maximum Copy Number'] = amplicon_df['Feature maximum copy number'].astype(float)
-            amplicon_df['Feature Median Copy Number'] = amplicon_df['Feature median copy number'].astype(float)
-            amplicon_df = amplicon_df.round(decimals=2)
-            for i in range(len(amplicon_df['AA amplicon number'].unique())):
-                number = amplicon_df['AA amplicon number'].unique()[i]
-                per_amplicon = amplicon_df[amplicon_df['AA amplicon number'] == number]
-                
-                show_legend = number not in seen
-                seen.add(number)
-                
-                fig.add_trace(go.Scatter(x = per_amplicon['Feature Position'], y = per_amplicon['Y-axis'], 
-                        customdata = amplicon_df, mode='lines',fill='tozeroy', hoveron='points+fills', hovertemplate=
-                        '<br><i>Feature Classification:</i> %{customdata[3]}<br>' + 
-                        '<i>%{customdata[16]}:</i> %{customdata[17]} - %{customdata[18]}<br>' +
-                        '<i>Oncogenes:</i> %{customdata[5]}<br>'+
-                        '<i>Feature Maximum Copy Number:</i> %{customdata[9]}<br>'
-                        f'<b class="/{project_data_dir}/AA_outputs/{sample_name}/{sample_name}_AA_results/{sample_name}_amplicon{number}.png">Click to Download Amplicon {number} PNG</b>',
-                        name = '<b>Amplicon ' + str(number) + '</b>', opacity = 0.3, fillcolor = amplicon_colors[amplicon_numbers.index(number)],
-                        line = dict(color = amplicon_colors[amplicon_numbers.index(number)]), showlegend=show_legend, legendrank=number, text='hallo'), row = rowind, col = colind)
-                fig.update_traces(textposition="bottom right")
+                    amplicon_df = pd.DataFrame()
+
         cent_df = pd.read_csv(cent_file, header = None, sep = '\t')
         #display(a_df)
         cent_df = cent_df[cent_df[0] == key]
@@ -174,12 +180,12 @@ def plot(sample, sample_name, project_name, filter_plots=True):
 
 
         if rowind == 1 and colind == 1:
-            fig.add_trace(go.Scatter(x = chr_df['Centromere Position'], y = chr_df['Y-axis'], fill = 'tozeroy', mode = 'lines', fillcolor = 'rgba(2, 6, 54, 0.15)', 
-                line_color = 'rgba(2, 6, 54, 0.3)', customdata = chr_df, hovertemplate = 
+            fig.add_trace(go.Scatter(x = chr_df['Centromere Position'], y = chr_df['Y-axis'], fill = 'tozeroy', mode = 'lines', fillcolor = 'rgba(2, 6, 54, 0.3)', 
+                line_color = 'rgba(2, 6, 54, 0.2)', customdata = chr_df, hovertemplate = 
                 '<br>%{customdata[0]}: %{customdata[1]}-%{customdata[2]}', name = 'Centromere', legendrank=0), row = rowind, col = colind)
         else:
-            fig.add_trace(go.Scatter(x = chr_df['Centromere Position'], y = chr_df['Y-axis'], fill = 'tozeroy', mode = 'lines', fillcolor = 'rgba(2, 6, 54, 0.15)', 
-                line_color = 'rgba(2, 6, 54, 0.3)', customdata = chr_df, name = 'Centromere', showlegend = False, hovertemplate = 
+            fig.add_trace(go.Scatter(x = chr_df['Centromere Position'], y = chr_df['Y-axis'], fill = 'tozeroy', mode = 'lines', fillcolor = 'rgba(2, 6, 54, 0.3)', 
+                line_color = 'rgba(2, 6, 54, 0.2)', customdata = chr_df, name = 'Centromere', showlegend = False, hovertemplate = 
                 '<br>%{customdata[0]}: %{customdata[1]}-%{customdata[2]}'
                 ), row = rowind, col = colind)
 
@@ -196,7 +202,7 @@ def plot(sample, sample_name, project_name, filter_plots=True):
                 ticklen = 10, showline = True, linewidth = 1, showgrid = False, range = [0,2], tick0 = 0, dtick = 1, tickmode = 'array', tickvals = [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100], 
                 ticksuffix = " ", row = rowind, col = colind)
         else:
-            fig.update_yaxes(autorange = False, ticks = 'outside', ticklen = 10, range = [0, 20], ticktext = ['0', '', '', '', '20'], tickvals = [0, 5, 10, 15, 20], showline = True, linewidth = 1, showgrid = False, 
+            fig.update_yaxes(autorange = False, ticks = 'outside', ticklen = 10, range = [0, 20], ticktext = ['0', '', '10', '', '20'], tickvals = [0, 5, 10, 15, 20], showline = True, linewidth = 1, showgrid = False, 
                 tick0 = 0, dtick = 1, tickmode = 'array', ticksuffix = " ", row = rowind, col = colind)
 
         if colind == 1:
