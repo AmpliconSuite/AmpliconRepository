2a807b14e1e5f3d750b732b27e0afa2abde3be80
Jens Luebeck <jluebeck@eng.ucsd.edu>
2023-04-05 13:53:00 -0700

    safer handling of download metadata when file missing

--- caper/caper/views.py
+++ caper/caper/views.py
@@ -386,12 +386,17 @@ def get_sample_metadata(sample_data):
 def sample_metadata_download(request, project_name, sample_name):
     project, sample_data = get_one_sample(project_name, sample_name)
     sample_metadata_id = sample_data[0]['Sample metadata JSON']
-    sample_metadata = fs_handle.get(ObjectId(sample_metadata_id)).read()
-    response = HttpResponse(sample_metadata)
-    response['Content-Type'] = 'application/json'
-    response['Content-Disposition'] = f'attachment; filename={sample_name}.json'
-    clear_tmp()
-    return response
+    try:
+        sample_metadata = fs_handle.get(ObjectId(sample_metadata_id)).read()
+        response = HttpResponse(sample_metadata)
+        response['Content-Type'] = 'application/json'
+        response['Content-Disposition'] = f'attachment; filename={sample_name}.json'
+        clear_tmp()
+        return response
+
+    except Exception as e:
+        print(e)
+        return HttpResponse()
 
 # @cache_page(600) # 10 minutes
 def sample_page(request, project_name, sample_name):
