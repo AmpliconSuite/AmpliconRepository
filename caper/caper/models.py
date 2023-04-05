79bb2b1652f32682b62237b99f13195c4bda42b8
Edwin Huang <43209173+edwin5588@users.noreply.github.com>
2023-04-05 16:14:10 -0700

    Revert "changed models.py to allow for blank project members during project creation"
    This reverts commit c2ad281a5d8eb4cd6002523e26b5c9a3a0ed7cb9.

--- caper/caper/models.py
+++ caper/caper/models.py
@@ -7,7 +7,7 @@ class Run(models.Model):
     project_name = models.CharField(max_length=1000)
     description = models.CharField(max_length=1000)
     private = models.BooleanField(default=False)
-    project_members = models.CharField(max_length=1000, blank = True)
+    project_members = models.CharField(max_length=1000)
 
 # class Feature(models.Model):
 #     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
