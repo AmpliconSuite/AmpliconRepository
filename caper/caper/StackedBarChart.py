777d098286749674779c33106c3499186c9c11f9
Rohil Ahuja <ahujar27@gmail.com>
2023-05-15 10:23:00 -0700

    horizontal scroll bar and tick angle

--- caper/caper/StackedBarChart.py
+++ caper/caper/StackedBarChart.py
@@ -44,16 +44,16 @@ def StackedBarChart(sample):
                         'Linear amplification' : 'rgb(27, 111, 185)'},
                  hover_data = {'Sample_name': False}, range_x=([-0.5, 9]))
 
-    fig.update_xaxes(tickangle=90, automargin=False, tickfont=dict(size=10), gridcolor = 'white', tickprefix = "   ")
-    fig.update_yaxes(gridcolor = 'white')
+    fig.update_xaxes(tickangle=60, automargin=True, tickfont=dict(size=10), gridcolor = 'white', tickprefix = "   ", rangeslider_visible=True)
+    fig.update_yaxes(gridcolor = 'white', rangemode='tozero')
     fig.update_layout(showlegend=False, plot_bgcolor = 'white')
     fig.update_layout(yaxis_title="Number of focal amps")
     fig.update_layout(xaxis_title=None)
-    fig.update_layout(height=400, margin={'t': 20, 'b': 80, 'r': 0, 'l': 20})
-    fig.update_traces(hovertemplate='%{y:} amps, ' + 'Sample: %{x}' + '<br><b></b>', cliponaxis = False)
+    fig.update_layout(height=400, margin={'t': 20, 'b': 0, 'r': 0, 'l': 20})
+    fig.update_traces(hovertemplate='%{y:} amps, ' + 'Sample: %{x}' + '<br><b></b>', cliponaxis = True)
 
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f"Created project barchart plot in {elapsed_time} seconds")
-    return fig.to_html(full_html=False, config={'modeBarButtonsToRemove': ['zoom']},
+    return fig.to_html(full_html=False, config={'displayModeBar': ['True']}, #'modeBarButtonsToRemove': ['zoom'],
                        div_id="project_bar_plotly_div")
