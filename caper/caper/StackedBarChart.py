44cd43128329dd4b0c5e69934f41f1b43f4b9dbb
Rohil Ahuja <ahujar27@gmail.com>
2023-05-16 12:29:01 -0700

    truncating long sample names

--- caper/caper/StackedBarChart.py
+++ caper/caper/StackedBarChart.py
@@ -2,7 +2,6 @@ import time
 import pandas as pd
 import plotly.express as px
 
-
 def StackedBarChart(sample):
     start_time = time.time()
     df = pd.DataFrame(sample)
@@ -24,6 +23,7 @@ def StackedBarChart(sample):
     output = df2.pivot(index='Sample_name', columns='Classification', values='Count')
     df = output.sort_values(classes, ascending=[False, False, False, False])
     df2 = df.reset_index()
+    df2['Sample_name'] = df2['Sample_name'].apply(lambda x: x[0:10] + "..." if len(x) > 10 else x)
 
     if len(df2['Sample_name']) < 10:
         fig = px.bar(df2, x="Sample_name", y = classes,
@@ -35,7 +35,7 @@ def StackedBarChart(sample):
                         'Linear amplification' : 'rgb(27, 111, 185)'},
                  hover_data = {'Sample_name': False})
     else:
-        fig = px.bar(df2, x="Sample_name", y = classes,
+        fig = px.bar(data_frame = df2, x="Sample_name", y = classes,
                 barmode = 'stack',
                  color_discrete_map = {
                         'ecDNA' : "rgb(255, 0, 0)",
@@ -44,14 +44,14 @@ def StackedBarChart(sample):
                         'Linear amplification' : 'rgb(27, 111, 185)'},
                  hover_data = {'Sample_name': False}, range_x=([-0.5, 9]))
 
-    fig.update_xaxes(tickangle=60, automargin=True, tickfont=dict(size=10), gridcolor = 'white', tickprefix = "   ", rangeslider_visible=True)
+    fig.update_xaxes(tickangle=60, automargin=True, tickfont=dict(size=10), gridcolor = 'white', rangeslider_visible=True, tickprefix = "   ")
     fig.update_yaxes(gridcolor = 'white', rangemode='tozero')
     fig.update_layout(showlegend=False, plot_bgcolor = 'white')
     fig.update_layout(yaxis_title="Number of focal amps")
     fig.update_layout(xaxis_title=None)
     fig.update_layout(height=400, margin={'t': 20, 'b': 0, 'r': 0, 'l': 20})
-    fig.update_traces(hovertemplate='%{y:} amps, ' + 'Sample: %{x}' + '<br><b></b>', cliponaxis = True)
-
+    fig.update_traces(hovertemplate='%{y}: amps, ' + 'Sample: %{x}' + '<br><b></b>', cliponaxis = True)
+    
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f"Created project barchart plot in {elapsed_time} seconds")
