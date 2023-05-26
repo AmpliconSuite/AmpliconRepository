d40cc5ad1fff855220868402340efc81b7be1237
Ubuntu <ubuntu@ip-172-31-85-178.ec2.internal>
2023-05-26 13:34:49 +0000

    move style.css to S3 as well.  Pass env var through start-server script

--- start-server.sh
+++ start-server.sh
@@ -3,5 +3,5 @@ source caper/config.sh
 #echo "/srv/caper/manage.py runserver 0.0.0.0:8000 &1>/srv/logs/stdout.txt &2>/srv/logs/stderr.txt"
 #docker run  --rm --name=amplicon-dev -p 8080:8000 -v /home/ubuntu/AmpliconRepository-dev/logs:/srv/logs  -v /home/ubuntu/AmpliconRepository-dev/caper:/srv/caper/ -w /srv/  --env GOOGLE_SECRET_KEY --env GLOBUS_SECRET_KEY --env DB_URI --env DB_NAME -it genepattern/amplicon-repo:dev1 bash
 
-docker run -d --rm  --name=amplicon-dev -p 8080:8000 -v /home/ubuntu/AmpliconRepository-dev/logs:/srv/logs -v /home/ubuntu/AmpliconRepository-dev/caper:/srv/caper/ -w /srv/caper/  --env GOOGLE_SECRET_KEY --env GLOBUS_SECRET_KEY --env DB_URI --env DB_NAME -t genepattern/amplicon-repo:dev /srv/run-manage-py.sh
+docker run -d --rm  --name=amplicon-dev -p 8080:8000 -v /home/ubuntu/AmpliconRepository-dev/logs:/srv/logs -v /home/ubuntu/AmpliconRepository-dev/caper:/srv/caper/ -w /srv/caper/  --env GOOGLE_SECRET_KEY --env GLOBUS_SECRET_KEY --env DB_URI --env DB_NAME --env S3_STATIC_FILES -t genepattern/amplicon-repo:dev /srv/run-manage-py.sh
 
