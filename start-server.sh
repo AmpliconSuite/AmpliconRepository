ee45ce819eaf44c05d8b0477010c0dc077828332
Ubuntu <ubuntu@ip-172-31-29-144.ec2.internal>
2023-06-02 13:40:32 +0000

    One more localization moved to config.sh

--- start-server.sh
+++ start-server.sh
@@ -7,7 +7,7 @@ source caper/config.sh
 #docker run -d --rm  --name=amplicon-prod -p 80:8000 -v /home/ubuntu/AmpliconRepository-prod/logs:/srv/logs -v /home/ubuntu/AmpliconRepository-prod/caper:/srv/caper/ -w /srv/  --env GOOGLE_SECRET_KEY --env GLOBUS_SECRET_KEY --env DB_URI --env DB_NAME  --env S3_STATIC_FILES -t genepattern/amplicon-repo:dev /srv/run-manage-py.sh
 
 #docker rm amplicon-prod
-docker run -d  --name=amplicon-${AMPLICON_ENV} -p ${AMPLICON_ENV_PORT}:8000 -v /home/ubuntu/.aws:/root/.aws  -v /home/ubuntu/AmpliconRepository-dev/logs:/srv/logs -v /home/ubuntu/AmpliconRepository-dev/caper:/srv/caper/ -w /srv/caper  --env GOOGLE_SECRET_KEY --env GLOBUS_SECRET_KEY --env DB_URI --env DB_NAME  --env S3_STATIC_FILES --env S3_FILE_DOWNLOADS -t genepattern/amplicon-repo:${AMPLICON_ENV} /srv/run-manage-py.sh
+docker run -d  --name=amplicon-${AMPLICON_ENV} -p ${AMPLICON_ENV_PORT}:8000 -v /home/ubuntu/.aws:/root/.aws  -v ${CAPER_ROOT}/logs:/srv/logs -v ${CAPER_ROOT}/caper:/srv/caper/ -w /srv/caper  --env GOOGLE_SECRET_KEY --env GLOBUS_SECRET_KEY --env DB_URI --env DB_NAME  --env S3_STATIC_FILES --env S3_FILE_DOWNLOADS -t genepattern/amplicon-repo:${AMPLICON_ENV} /srv/run-manage-py.sh
 
 
 
