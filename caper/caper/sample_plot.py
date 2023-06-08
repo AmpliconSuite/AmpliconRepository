f8d299012b62a2b6b1a163ea708222aec7a8a0b8
Jens Luebeck <jluebeck@eng.ucsd.edu>
2023-06-07 21:51:57 -0700

    add support for mm10

--- caper/caper/sample_plot.py
+++ caper/caper/sample_plot.py
@@ -118,7 +118,15 @@ def plot(sample, sample_name, project_name, filter_plots=False):
             chromosomes = []
 
     else:
-        chromosomes = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "X", "Y")
+        if ref == "mm10":
+            chromosomes = (
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+            "X", "Y")
+        else:
+            chromosomes = (
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+            "20", "21", "22", "X", "Y")
+
 
     n_amps = len(amplicon_numbers)
     cmap = cm.get_cmap('Spectral', n_amps + 2)
