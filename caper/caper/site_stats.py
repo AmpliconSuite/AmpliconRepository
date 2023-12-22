e6d50d16a74530ed10c9994ba64b7da4c9540c29
ted Liefeld <liefeld@broadinstitute.org>
2023-12-22 11:06:11 -0800

    added stats for the major aplicon classifications

--- caper/caper/site_stats.py
+++ caper/caper/site_stats.py
@@ -1,20 +1,46 @@
 import datetime
 
-from .utils import get_collection_handle, collection_handle, db_handle
-
+from .utils import get_collection_handle, collection_handle, db_handle, replace_space_to_underscore, preprocess_sample_data, get_one_sample, sample_data_from_feature_list
 
 site_statistics_handle = get_collection_handle(db_handle,'site_statistics')
 
 def get_latest_site_statistics():
     return site_statistics_handle.find().sort('_id', -1).limit(1).next()
 
+
+def sum_amplicon_counts_by_classification(class_keys, class_values, sum_holder):
+    for class_name in class_keys:
+        prev = 0
+        if sum_holder.get(class_name) != None:
+            prev = sum_holder.get(class_name)
+
+        sum_holder[class_name] = prev + class_values.get(class_name)
+    return sum_holder
+
+def subtract_amplicon_counts_by_classification(class_keys, class_values, sum_holder):
+    for class_name in class_keys:
+        prev = 0
+        if sum_holder.get(class_name) != None:
+            prev = sum_holder.get(class_name)
+
+        val = prev - class_values.get(class_name)
+        if (val < 0):
+            print(" NEGATIVE AMPOLICON COUNTS IN SITE STATS AFTER PROJECT DELETION -- SHOULD REGENRATE ALL SITE STATS")
+            val = 0
+        sum_holder[class_name] = val
+    return sum_holder
+
 def regenerate_site_statistics():
+    pub_amplicon_counts = dict()
+    priv_amplicon_counts = dict()
 
     # just get stats for all private
     all_private_proj_count = 0
     all_private_sample_count = 0
     all_private_projects = list(collection_handle.find({'private': True, 'delete': False}))
     for proj in all_private_projects:
+        class_keys, amplicon_counts = get_project_amplicon_counts(proj)
+        sum_amplicon_counts_by_classification(class_keys, amplicon_counts, priv_amplicon_counts)
         all_private_proj_count = all_private_proj_count + 1
         all_private_sample_count = all_private_sample_count + len(proj['runs'])
     # end private stats
@@ -23,19 +49,29 @@ def regenerate_site_statistics():
     public_sample_count = 0
     public_projects = list(collection_handle.find({'private': False, 'delete': False}))
     for proj in public_projects:
+        class_keys, amplicon_counts = get_project_amplicon_counts(proj)
+        sum_amplicon_counts_by_classification(class_keys, amplicon_counts, pub_amplicon_counts)
         public_proj_count = public_proj_count + 1
         public_sample_count = public_sample_count + len(proj['runs'])
 
+    print(f"  pub amp counts is  == {pub_amplicon_counts} ")
     # make it an array of name/values so we can add to it later
     repo_stats = {}
     repo_stats["public_proj_count"] = public_proj_count
     repo_stats["public_sample_count"] = public_sample_count
     repo_stats["all_private_proj_count"] = all_private_proj_count
     repo_stats["all_private_sample_count"] = all_private_sample_count
+    repo_stats["all_private_amplicon_classifications_count"] = priv_amplicon_counts
+    repo_stats["public_amplicon_classifications_count"] = pub_amplicon_counts
+
     repo_stats["date"] = datetime.datetime.today()
     new_id = site_statistics_handle.insert_one(repo_stats)
     #print(site_statistics_handle.count_documents({}))
-    print("SITE STATS REGENERATED FROM SCRATCH")
+    print(f"SITE STATS REGENERATED FROM SCRATCH    {pub_amplicon_counts} ")
+
+
+
+
     return repo_stats
 
 #
@@ -55,10 +91,21 @@ def add_project_to_site_statistics(project):
         # private proj, don't change public stats
         updated_stats["public_proj_count"] = current_stats["public_proj_count"]
         updated_stats["public_sample_count"] = current_stats["public_proj_count"]
+        priv_amplicon_counts = current_stats["all_private_amplicon_classifications_count"]
+        class_keys, amplicon_counts = get_project_amplicon_counts(project)
+        sum_amplicon_counts_by_classification(class_keys, amplicon_counts, priv_amplicon_counts)
+        updated_stats["all_private_amplicon_classifications_count"] = priv_amplicon_counts
+        updated_stats["public_amplicon_classifications_count"]=current_stats["public_amplicon_classifications_count"]
     else:
         # change to public
         updated_stats["public_proj_count"] = current_stats["public_proj_count"] + 1
         updated_stats["public_sample_count"] = current_stats["public_proj_count"] + len(project['runs'])
+        pub_amplicon_counts = current_stats["public_amplicon_classifications_count"]
+        class_keys, amplicon_counts = get_project_amplicon_counts(project)
+        sum_amplicon_counts_by_classification(class_keys, amplicon_counts, pub_amplicon_counts)
+        updated_stats["public_amplicon_classifications_count"] = pub_amplicon_counts
+        updated_stats["all_private_amplicon_classifications_count"] = current_stats["all_private_amplicon_classifications_count"]
+
 
     updated_stats["date"] = datetime.datetime.today()
     new_id = site_statistics_handle.insert_one(updated_stats)
@@ -82,3 +129,34 @@ def delete_project_from_site_statistics(project):
     print(f"DELETE                                 updated {updated_stats} ")
     updated_stats["date"] = datetime.datetime.today()
     new_id = site_statistics_handle.insert_one(updated_stats)
+
+
+
+def get_project_amplicon_counts(project):
+    project_linkid = project['_id']
+    amplicon_counts = dict()
+    class_keys = set()
+    runs = project['runs']
+    for sample_num in runs.keys():
+        features = runs[sample_num]
+        for feat in features:
+            #print(f"FEATURE: {feat} ")
+            classification = feat['Classification']
+            if (classification == None):
+                classification = "Unclassified"
+            class_counts = 0
+            if (amplicon_counts.get(classification) != None):
+                class_counts = amplicon_counts.get(classification)
+
+            amplicon_counts[classification] = class_counts +1
+            class_keys.add(classification)
+
+            # temporary (?) hack to not have spaces to make using this in the django templates easier
+            if classification == 'Complex non-cyclic':
+                amplicon_counts['Complex_non_cyclic'] = amplicon_counts['Complex non-cyclic']
+                class_keys.add('Complex_non_cyclic')
+            if classification == 'Linear amplification':
+                amplicon_counts['Linear_amplification'] = amplicon_counts['Linear amplification']
+                class_keys.add('Linear_amplification')
+
+    return class_keys, amplicon_counts
--- caper/caper/views.py
+++ caper/caper/views.py
@@ -25,7 +25,9 @@ import json
 
 # from .models import File
 from .forms import RunForm, UpdateForm, FeaturedProjectForm, DeletedProjectForm
-from .utils import get_db_handle, collection_handle, db_handle, fs_handle
+from .utils import collection_handle, db_handle, fs_handle, replace_space_to_underscore, \
+    preprocess_sample_data, get_one_sample, sample_data_from_feature_list, get_one_project, validate_project, \
+    prepare_project_linkid, replace_underscore_keys
 from django.forms.models import model_to_dict
 
 import subprocess
@@ -85,49 +87,6 @@ def get_date():
     return date
 
 
-def prepare_project_linkid(project):
-    project['linkid'] = project['_id']
-
-
-def get_one_project(project_name_or_uuid):
-    try:
-        project = collection_handle.find({'_id': ObjectId(project_name_or_uuid), 'delete': False})[0]
-        prepare_project_linkid(project)
-        return project
-
-    except:
-        project = None    
-
-    # backstop using the name the old way
-    if project is None:
-        try:
-            project = collection_handle.find_one({'project_name': project_name_or_uuid, 'delete': False})
-            if project is not None:
-                logging.warning(f"Could not lookup project {project_name_or_uuid}, had to use project name!")
-                prepare_project_linkid(project)
-                return project
-        except:
-            project = None
-
-    # look for project that has been versioned by editing and giving it a new file.  This makes sure that
-    # the old links work
-    if project is None:
-        try:
-            project = collection_handle.find_one({'previous_project_ids': project_name_or_uuid, 'delete': False})
-            if project is not None:
-                prepare_project_linkid(project)
-                logging.warning(f"Could not lookup project {project_name_or_uuid}, had to use previous project ids!")
-
-                return project
-        except:
-            project = None
-
-    if project is None:
-        logging.error(f"Project is None for {project_name_or_uuid}")
-
-    return project
-
-
 def get_one_deleted_project(project_name_or_uuid):
     try:
         project = collection_handle.find({'_id': ObjectId(project_name_or_uuid), 'delete': True})[0]
@@ -147,21 +106,6 @@ def get_one_deleted_project(project_name_or_uuid):
         logging.error(f"Project is None for {project_name_or_uuid}")
 
     return project
-
-
-def get_one_sample(project_name, sample_name):
-    project = validate_project(get_one_project(project_name), project_name)
-    # print("ID --- ", project['_id'])
-    runs = project['runs']
-    for sample_num in runs.keys():
-        current = runs[sample_num]
-        try:
-            if len(current) > 0 and current[0]['Sample_name'] == sample_name:
-                sample_out = current
-        except:
-            # should not get here but we do sometimes for new projects, issue 194
-            sample_out = None
-    return project, sample_out
 
 
 def get_one_feature(project_name, sample_name, feature_name):
@@ -200,103 +144,6 @@ def form_to_dict(form):
     run = form.save(commit=False)
     form_dict = model_to_dict(run)
     return form_dict
-
-
-def flatten(nested, lst = True, sort = True):
-    """
-    recursive function to get elements in nested list
-    """
-    flat = []
-    def helper(nested):
-        for e in nested:
-            if isinstance(e, list):
-                helper(e)
-            else:
-                if e:
-                    e = e.replace("'",'')
-                    if len(e) > 0:
-                        flat.append(e)
-    helper(nested)
-    
-    if lst and sort:
-        return list(sorted(flat))
-    return flat
-
-
-def sample_data_from_feature_list(features_list):
-    """
-    extracts sample data from a list of features
-    """
-    # now = datetime.datetime.now()
-    df = pd.DataFrame(features_list)[['Sample_name', 'Oncogenes', 'Classification', 'Feature_ID']]
-    sample_data = []
-    for sample_name, indices in df.groupby(['Sample_name']).groups.items():
-        sample_dict = dict()
-        subset = df.iloc[indices]
-        sample_dict['Sample_name'] = sample_name
-        sample_dict['Oncogenes'] = sorted(set(flatten(subset['Oncogenes'].values.tolist())))
-        sample_dict['Classifications'] = list(set(flatten(subset['Classification'].values.tolist())))
-        if len(sample_dict['Oncogenes']) == 0 and len(sample_dict['Classifications']) == 0:
-            sample_dict['Features'] = 0
-        else:
-            sample_dict['Features'] = len(subset['Feature_ID'])
-        sample_data.append(sample_dict)
-    # print(f'********** TOOK {datetime.datetime.now() - now}')
-    return sample_data
-
-
-def replace_space_to_underscore(runs):
-    '''
-    Replaces all spaces to underscores
-    '''
-        
-    if type(runs) == dict:
-        run_list = []
-        for run in runs:
-            for sample in runs[run]:
-                for key in list(sample.keys()):
-                    newkey = key.replace(" ", "_")
-                    sample[newkey] = sample.pop(key)
-                run_list.append(sample)
-        return run_list
-
-    else:
-        run_list = []
-        for sample in runs:
-            run_list.append({})
-            for key in list(sample.keys()):
-                newkey = key.replace(" ", "_")
-                run_list[-1][newkey] = sample[key]
-
-        return run_list
-
-def preprocess_sample_data(sample_data, copy=True, decimal_place=2):
-    if copy:
-        sample_data = [feature.copy() for feature in sample_data]
-
-    # sample_data.sort(key=lambda x: (int(x['AA_amplicon_number']), x['Feature_ID']))
-    for feature in sample_data:
-        for key, value in feature.items():
-            if type(value) == float:
-                if key == 'AA_amplicon_number':
-                    feature[key] = int(value)
-
-                else:
-                    feature[key] = round(value, 1)
-
-            elif type(value) == str and value.startswith('['):
-                feature[key] = ', \n'.join(value[2:-2].split("', '"))
-
-            else:
-                feature[key] = value
-
-        locations = [i.replace("'","").strip() for i in feature['Location']]
-        feature['Location'] = locations
-        oncogenes = [i.replace("'","").strip() for i in feature['Oncogenes']]
-        feature['Oncogenes'] = oncogenes
-        
-    # print(sample_data[0])
-    return sample_data
 
 
 def get_project_oncogenes(runs):
@@ -502,48 +349,6 @@ def create_aggregate_df(project, samples):
     logging.info(f"Iteratively build project dataframe from samples in {diff} seconds")
     
     return aggregate, aggregate_save_fp
-
-
-def replace_underscore_keys(runs_from_proj_creation):
-    """
-    Replaces underscores in the keys from runs at proj creation step
-    """
-    new_run = {}
-    for sample in runs_from_proj_creation.keys():
-        features = []
-        for feature in runs_from_proj_creation[sample]:
-            new_feat = {}
-            for key in feature.keys():
-                new_feat[key.replace(" ", '_')] = feature[key]
-            features.append(new_feat)
-        new_run[sample] = features
-    return new_run
-
-
-def validate_project(project, project_name):
-    """
-    Checks the following for a project: 
-    1. if keys in project[runs] all contain underscores, if not, replace them with underscores, insert into db
-    """
-
-    ## check for 1
-    update = False
-    for sample in project['runs'].keys():
-        for feature in project['runs'][sample]:
-            for key in feature.keys():
-                if ' ' in key:
-                    runs = replace_underscore_keys(project['runs'])
-                    update = True
-                    break
-    if update:
-        new_values = {"$set": {
-            'runs': runs
-        }}
-        query = {'_id': project['_id'],
-                    'delete': False}
-        collection_handle.update(query, new_values)
-
-    return get_one_project(project_name)
 
 
 def project_page(request, project_name, message=''):
