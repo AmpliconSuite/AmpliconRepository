2f654687377450cc41099d7186b91bbef482d729
Edwin Huang <43209173+edwin5588@users.noreply.github.com>
2024-05-29 12:50:47 -0700

    Revert "modified project view statistic to depend on sessions."
    This reverts commit 30366b7fcbdf28450b409c247b913956c44c12c6.

--- caper/caper/view_download_stats.py
+++ caper/caper/view_download_stats.py
@@ -2,30 +2,10 @@ from .utils import *
 
 
 
-def session_visit(request, project):
-    """
-    If the user session hasn't viewed that project page yet, then record it.
-    If it has visited, don't increment. 
-    
-    """
-    ## if the user session hasn't visited the project page yet, increment. 
-    proj_id = project['_id']
-    if (request.session.get(f'visited_{proj_id}') is None) or (request.session.get(f'visited_{proj_id}') == False):
-        ## increment:
-        
-        request.session[f'visited_{proj_id}'] = True
-        
-        return get_increment_view_and_download_statistics(project)
-    else:
-        ## only get current stats
-        query = {'_id':project['_id']}
-        res = collection_handle.find_one(query, {'views':1, 'downloads':1})
-        return [res.get('views'), res.get('downloads')]
-    
 def get_increment_view_and_download_statistics(project):
     
     """
-    Gets the view and download statistics for a project. 
+    Gets the view and download statistics for a project, adds 1, and returns it. 
     
     """
     query = {'_id':project['_id']}
@@ -59,3 +39,4 @@ def increment_download(project):
             'downloads':1
         }
     })
+    
