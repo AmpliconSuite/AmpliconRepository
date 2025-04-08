7292dd24e545377419b4e2854059b8f089041f8e
liefeld <liefeld@broadinstitute.org>
2025-04-08 08:21:50 -0700

    Update start-neo4j-container.sh
    added mount of conf directory, specific version for neo4j and removed hardcoded 'dev' from path replacing it with '${AMPLICON_ENV}' from the localized config.sh

--- start-neo4j-container.sh
+++ start-neo4j-container.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #docker run -d --name neo4j -p 7474:7474 -p 7687:7687 --env NEO4J_AUTH=neo4j/$NEO4J_PASSWORD_SECRET -v /home/ubuntu/AmpliconRepository-dev/neo4j neo4j
+. ./caper/config.sh
 
-docker run -d --name neo4j -p 7474:7474 -p 7687:7687 --env NEO4J_AUTH=neo4j/$NEO4J_PASSWORD_SECRET -v /home/ubuntu/AmpliconRepository-dev/neo4j neo4j
+docker run -d --name neo4j -p 7474:7474 -p 7687:7687 --env NEO4J_AUTH=neo4j/$NEO4J_PASSWORD_SECRET ---volume=/home/ubuntu/AmpliconRepository-${AMPLICON_ENV}/neo4j/conf:/conf neo4j:2025.03.0
 
