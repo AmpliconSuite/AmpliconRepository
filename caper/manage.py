1beac934ea27fb66749864bc3cddef430117f452
liefeld <liefeld@broadinstitute.org>
2025-07-07 09:19:15 -0700

    Attempt to make shutdown more reliable and prevent the case where django server keeps running on aws host even though the docker container is gone

--- caper/manage.py
+++ caper/manage.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 import os
 import sys
+import signal
+
+def sighandler(signum, frame):
+    sys.exit(1)
 
 if __name__ == "__main__":
-
+    signal.signal(signal.SIGTERM, sighandler)
+    signal.signal(signal.SIGINT, sighandler)
+    
     from mezzanine.utils.conf import real_project_name
 
     settings_module = "%s.settings" % real_project_name("caper")
