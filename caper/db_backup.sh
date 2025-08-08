2f337aaae950b902f0f93997c711e3edb9614102
liefeld <liefeld@broadinstitute.org>
2025-08-08 06:58:58 -0700

    db backup first pass

---
+++ caper/db_backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source ./caper/config.sh
+
+DATE=$(date +%Y%m%d)
+BACKUP_DIR="../backups"
+DB_PATH="./caper.sqlite3"
+
+# Create backup with timestamp
+sqlite3 $DB_PATH ".backup $BACKUP_DIR/backup_$DATE.sqlite3"
+
+# Compress the backup
+gzip $BACKUP_DIR/backup_$DATE.sqlite3
+
+# Copy to off-site storage (example using AWS S3)
+aws s3 cp $BACKUP_DIR/backup_$DATE.sqlite3.gz s3://amprepo-backups/prod/sqlite/
