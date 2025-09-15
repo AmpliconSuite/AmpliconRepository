22b943a060299d2a6cec1f164cba6820d22da480
liefeld <liefeld@broadinstitute.org>
2025-09-15 14:26:15 -0700

    fix indent

--- caper/caper/settings.py
+++ caper/caper/settings.py
@@ -6,18 +6,18 @@ caper_log_level_name = os.getenv("CAPER_LOG_LEVEL", "INFO").upper()
 
 try:
     default_log_level = getattr(logging, default_log_level_name)
-    except AttributeError:
-        print(f"Warning: Invalid default log level '{default_log_level_name}' specified. Defaulting to ERROR.")
-        default_log_level = logging.ERROR
+except AttributeError:
+    print(f"Warning: Invalid default log level '{default_log_level_name}' specified. Defaulting to ERROR.")
+    default_log_level = logging.ERROR
 
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
                     level=default_log_level, datefmt='%Y-%m-%d %H:%M:%S')
 
 try:
     caper_log_level = getattr(logging, caper_log_level_name)
-    except AttributeError:
-        print(f"Warning: Invalid caper log level '{caper_log_level_name}' specified. Defaulting to DEBUG.")
-        caper_log_level = logging.DEBUG
+except AttributeError:
+    print(f"Warning: Invalid caper log level '{caper_log_level_name}' specified. Defaulting to DEBUG.")
+    caper_log_level = logging.DEBUG
 
 logger = logging.getLogger(__name__) 
 logger.setLevel(caper_log_level)
